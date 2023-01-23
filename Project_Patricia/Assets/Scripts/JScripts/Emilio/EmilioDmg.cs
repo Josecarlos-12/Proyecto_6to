974c642e6f0234506f3f4c86efa41b2493c5b5d0
Josecarlos-12 <85040568+Josecarlos-12@users.noreply.github.com>
2023-01-23 13:47:04 -0500

    Emilio damage
    Mike ya puede recibir daño de Emilio

--- Project_Patricia/Assets/Scripts/JScripts/Emilio/EmilioDmg.cs
+++ Project_Patricia/Assets/Scripts/JScripts/Emilio/EmilioDmg.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class EmilioDmg : MonoBehaviour
 {
+    public GameObject player;
+    private float damageRange;
+    public float damageset = 15f;
+    public float minDamage;
+    public float maxDamage;
+
+    public bool randomDamage;
+    public bool setDamage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        damageRange = Random.Range(minDamage, maxDamage);
+    }
+
+    void OntriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player" && randomDamage )
+        {
+            player.GetComponent<PlayerHealth>().sanity -= damageRange;
+        }
+
+        if(other.gameObject.tag=="Player"&& setDamage)
+        {
+            player.GetComponent<PlayerHealth>().sanity -= damageRange;
+        }
     }
 
     // Update is called once per frame
