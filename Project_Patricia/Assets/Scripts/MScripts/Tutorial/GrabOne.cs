b75d491c100cc8e8f6b48269622af3711d3a59b4
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2023-03-26 18:05:53 -0500

    Mejorando Tutorial

---
+++ Project_Patricia/Assets/Scripts/MScripts/Tutorial/GrabOne.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabOne : MonoBehaviour
+{
+    [SerializeField] private TrashOn[] trash;
+    [SerializeField] private GameObject grabTuto;
+    [SerializeField] private int count;
+
+    void Update()
+    {
+        if (trash[0].countPlayer == 1 || trash[1].countPlayer == 1 || trash[2].countPlayer == 1 || trash[3].countPlayer == 1 || trash[4].countPlayer == 1 || trash[5].countPlayer == 1)
+        {
+            if (count < 3)
+                count++;
+        }
+
+        if(count == 1)
+        {
+            grabTuto.SetActive(true);
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void ButtonAccept()
+    {
+        count = 3;
+
+        grabTuto.SetActive(false);
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
