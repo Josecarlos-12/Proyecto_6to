800d02c915c2e0521f601bf1d3418e24d5a2f696
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2023-04-26 16:43:11 -0500

    Mejorando boss nivel 1
    Vida _ Animación de muerte

--- Project_Patricia/Assets/Scripts/MScripts/Boss/TPBossLevel1.cs
+++ Project_Patricia/Assets/Scripts/MScripts/Boss/TPBossLevel1.cs
@@ -30,14 +30,17 @@ public class TPBossLevel1 : MonoBehaviour
     [SerializeField] private GameObject text;
 
     [Header("TP")]
-    [SerializeField] GameObject[] points;
+    [SerializeField] GameObject[] points, pointsSeventy, pointsFourty, pointsTen;
     [SerializeField] int despoint;
     [SerializeField] int tp, punch, damage, countOne;
 
     [Header("Punch")]
     [SerializeField] Transform playerBack;
-    [SerializeField] Animator anim;
+    [SerializeField] Animator anim, animCam;
     [SerializeField] bool bPunch;
+
+    [Header("CountLife")]
+    [SerializeField] int one, two, three;
 
     void Update()
     {
@@ -73,6 +76,143 @@ public class TPBossLevel1 : MonoBehaviour
                 {
                     Punch();                    
                 }
+            }
+        }
+
+        if (life < 70 && life > 40)
+        {
+            if(one<3)
+            one++;
+
+            if (one == 1)
+            {
+                despoint = 0;
+            }
+
+
+
+
+            if (countOne <= 4)
+            {
+                transform.position = pointsSeventy[despoint].transform.position;
+
+                if (tp < 3)
+                    tp++;
+
+                if (tp == 1)
+                {
+                    StartCoroutine("TPCorutine");
+                }
+            }
+            else if (countOne > 3)
+            {
+                change = false;
+                StopCoroutine("TPCorutine");
+
+                if (punch < 3)
+                    punch++;
+
+                if (punch == 1)
+                {
+                    Punch();
+                }
+            }
+        }
+
+        if (life < 40 && life > 10)
+        {
+            if (two < 3)
+                two++;
+
+            if (two == 1)
+            {
+                despoint = 0;
+            }
+
+
+
+            if (countOne <= 2)
+            {
+                transform.position = pointsFourty[despoint].transform.position;
+
+                if (tp < 3)
+                    tp++;
+
+                if (tp == 1)
+                {
+                    StartCoroutine("TPCorutine");
+                }
+            }
+            else if (countOne > 1)
+            {
+                change = false;
+                StopCoroutine("TPCorutine");
+
+                if (punch < 3)
+                    punch++;
+
+                if (punch == 1)
+                {
+                    Punch();
+                }
+            }
+        }
+
+        if (life < 10 && life > 1)
+        {
+            if (three < 3)
+                three++;
+
+            if (three == 1)
+            {
+                three = 0;
+            }
+
+
+            if (countOne <= 1)
+            {
+                transform.position = pointsTen[despoint].transform.position;
+
+                if (tp < 3)
+                    tp++;
+
+                if (tp == 1)
+                {
+                    StartCoroutine("TPCorutine");
+                }
+            }
+            else if (countOne > 0)
+            {
+                change = false;
+                StopCoroutine("TPCorutine");
+
+                if (punch < 3)
+                    punch++;
+
+                if (punch == 1)
+                {
+                    Punch();
+                }
+            }
+        }
+
+        if (life < 1)
+        {
+            agent.speed = 0;
+            if (countFinal < 3)
+                countFinal++;
+
+            if (countFinal == 1)
+            {
+                anim.SetBool("Run", false);
+                anim.SetBool("Punch", false);
+                agent.speed = 0;
+                myAlpha = 1;
+                animCam.SetBool("Final", true);
+                prota.SetActive(false);
+                cam.SetActive(true);
+                prota.transform.position = pos.transform.position;
+                prota.transform.rotation = pos.transform.rotation;
             }
         }
     }
@@ -116,8 +256,26 @@ public class TPBossLevel1 : MonoBehaviour
         myAlpha = 1;
         change = true;
         yield return new WaitForSeconds(4);
+        if (life > 70)
+        {
+            despoint = (despoint + 1) % points.Length;
+        }
+        if (life < 70 && life > 40)
+        {
+            despoint = (despoint + 1) % pointsSeventy.Length;
+        }
+        if (life < 40 && life > 10)
+        {
+            despoint = (despoint + 1) % pointsFourty.Length;
+        }
+        if (life < 10 && life > 1)
+        {
+            despoint = (despoint + 1) % pointsTen.Length;
+        }
+
+
         change = false;
-        despoint = (despoint + 1) % points.Length;
+        
         countOne++;
         yield return TPCorutine();
     }
@@ -199,5 +357,10 @@ public class TPBossLevel1 : MonoBehaviour
         yield return new WaitForSeconds(1);
         text.SetActive(false);
         Destroy(containerBoss);
+    }
+
+    public void ReduceTra()
+    {
+        change = true; 
     }
 }
