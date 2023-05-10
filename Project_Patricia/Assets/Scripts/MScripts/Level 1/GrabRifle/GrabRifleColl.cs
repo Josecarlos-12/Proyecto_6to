d15fc0204c7aad7a1a403c325038b9c382d1c62b
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2023-05-09 21:20:15 -0500

    Candado
    Puse el modelado de cadena en el tutorial, nivel 1 y nivel 2.

--- Project_Patricia/Assets/Scripts/MScripts/Level 1/GrabRifle/GrabRifleColl.cs
+++ Project_Patricia/Assets/Scripts/MScripts/Level 1/GrabRifle/GrabRifleColl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GrabRifleColl : MonoBehaviour
 {
-    public GameObject cam, prota, capsuleBlack, positon;
+    public GameObject cam, prota, capsuleBlack, positon, chain;
     [SerializeField] private Animator door;
     [SerializeField] private int count;
     [SerializeField] private Animator doorBoss;
@@ -16,8 +16,9 @@ public class GrabRifleColl : MonoBehaviour
             if(count<3)
             count++;
 
-            if (count == 1)
+            if (count == 1)                
             {
+                chain.SetActive(false);
                 door.SetBool("Open", true);
                 prota.SetActive(false);
                 cam.SetActive(true);
