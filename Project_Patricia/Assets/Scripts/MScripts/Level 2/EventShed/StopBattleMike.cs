471957f2db696c7f05a62dfca8b2a53faa641c5e
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2023-07-03 21:29:06 -0500

    Desactive Modo Sueño
    Hice que en el nivel 2 se desactive el modo sueño después de escapar de shadow mike

--- Project_Patricia/Assets/Scripts/MScripts/Level 2/EventShed/StopBattleMike.cs
+++ Project_Patricia/Assets/Scripts/MScripts/Level 2/EventShed/StopBattleMike.cs
@@ -8,6 +8,11 @@ public class StopBattleMike : MonoBehaviour
     [SerializeField] private GameObject otherCol;
     [SerializeField] private MikeBossLevel2 life;
     [SerializeField] private AudioSource sound3;
+    [SerializeField] private GameObject eyes;
+    [SerializeField] private WakingUpMode waking;
+    [SerializeField] private Collider col;
+    [SerializeField] private GameObject rifle, aim;
+    [SerializeField] private Weapon weapon;
 
     private void Start()
     {
@@ -18,12 +23,29 @@ public class StopBattleMike : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            col.enabled= false;
             sound3.Stop();
             mike.SetActive(true);
             Destroy(otherCol);
             life.life = 0;
-            Destroy(this.gameObject);
+            
+            StartCoroutine("FalseModeDream");
         }
+    }
+
+    public IEnumerator FalseModeDream()
+    {
+        yield return new WaitForSeconds(4.3f);
+        eyes.SetActive(true);
+        yield return new WaitForSeconds(0.48f);
+        rifle.SetActive(false);
+        aim.SetActive(false);
+        weapon.shoot= false;
+        weapon.shootTwo= false;
+        waking.WakingOff();
+        yield return new WaitForSeconds(1.12f);
+        eyes.SetActive(false);
+        Destroy(this.gameObject);
     }
 
 }
