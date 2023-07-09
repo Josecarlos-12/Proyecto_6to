2a3928356688890068024fe95c59ed63e1951f17
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2023-07-08 23:25:43 -0500

    Animación Tutorial

--- Project_Patricia/Assets/Scripts/MScripts/Tutorial/AnimPunch.cs
+++ Project_Patricia/Assets/Scripts/MScripts/Tutorial/AnimPunch.cs
@@ -1,40 +1,112 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class AnimPunch : MonoBehaviour
 {
-    [SerializeField] private GameObject cam, prota, panel, chapter1;
-    [SerializeField] private AudioSource chapterAudio, horror;
-    [SerializeField] private AudioClip clip;
+    [SerializeField] private GameObject dialogue, eText, prota, cam, activeCol, panel, chapter;
+    [SerializeField] private AudioSource audioChapter, punch, punch2;
+    [SerializeField] private Collider col;
+    [SerializeField] private bool into;
+    [SerializeField] private Animator animDoor;
 
+    public enum States
+    {
+        one, two
+    }
+    public States states;
+
+    private void Update()
+    {
+        switch (states)
+        {
+            case States.one:
+
+                break;
+            case States.two:
+                if(into && Input.GetKeyDown(KeyCode.E))
+                {
+                    animDoor.SetBool("Behind", true);
+                    prota.SetActive(false);
+                    cam.SetActive(true);
+                    col.enabled = false;
+                    into = false;
+                    eText.SetActive(false);
+                }
+                break;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            prota.SetActive(false);
-            cam.SetActive(true);
-            horror.Play();
-            StartCoroutine("ChapterOne");
+            switch (states)
+            {
+                case States.one:
+                    col.enabled = false;
+                    StartCoroutine("ChapterOne");
+                    break;
+                case States.two:
+                    into = true;
+                    eText.SetActive(true);
+                    break;
+            }
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            switch (states)
+            {
+                case States.one:
+
+                    break;
+                case States.two:
+                    into = false;
+                    eText.SetActive(false);
+                    break;
+            }
+        }   
     }
 
     public IEnumerator ChapterOne()
     {
-        yield return new WaitForSeconds(2.4f);
+        dialogue.SetActive(true);
+        dialogue.GetComponent<TextMeshProUGUI>().text = "Mike Schmith: Cat debe estar en el cuarto de Charlie cuidándolo";
+        yield return new WaitForSeconds(3f);
+        dialogue.SetActive(false);
+        activeCol.SetActive(true);
+        Destroy(gameObject);
+    }
+
+    public IEnumerator Dialogue()
+    {
+        dialogue.SetActive(true);
+        dialogue.GetComponent<TextMeshProUGUI>().text = "Mike Schmith: ¿Cat? ¿Charlie?";
+        yield return new WaitForSeconds(3f);
+        dialogue.SetActive(false);
+    }
+
+    public IEnumerator Final()
+    {
         panel.SetActive(true);
         yield return new WaitForSeconds(2f);
-        chapter1.SetActive(true);
-        chapterAudio.Play();
+        audioChapter.Play();
+        chapter.SetActive(true);
         yield return new WaitForSeconds(3f);
         SceneManager.LoadScene("Nivel 1");
     }
 
-    public void Punch()
+    public IEnumerator Punch()
     {
-        horror.clip = clip;
-        horror.Play();
+        punch2.Play();
+        yield return new WaitForSeconds(0.2f);
+        punch.Play();
     }
+
 }
